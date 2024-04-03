0b1468c4d32093f1b1f4ae9a4849cf7bcf000fdf
Jakob Rangel <jakmrang@iu.edu>
2024-04-03 01:42:48 -0400

    In Class 8 Completed

--- src/main/java/edu/iu/habahram/ducksservice/model/DuckData.java
+++ src/main/java/edu/iu/habahram/ducksservice/model/DuckData.java
@@ -1,18 +1,25 @@
 package edu.iu.habahram.ducksservice.model;
 
-public record DuckData(int id, String name, String type) {
-
-    public String toLine() {
-        return String.format("%1$s,%2$s,%3$s", id(), name(), type());
-    }
-
-    public String toLine(int id) {
-        return String.format("%1$s,%2$s,%3$s", id, name(), type());
-    }
+public record DuckData(int id, String type) implements Comparable {
 
     public static DuckData fromLine(String line) {
         String[] tokens = line.split(",");
-        return new DuckData(Integer.parseInt(tokens[0]), tokens[1], tokens[2]);
+        return new DuckData(Integer.parseInt(tokens[0]), tokens[1]);
+    }
+
+    public String toLine() {
+        return String.format("%1$s,%2$s", id(), type());
+    }
+
+    public int compareTo(Object object) {
+        DuckData otherDuck = (DuckData) object;
+        if (this.type().compareTo(otherDuck.type()) < 0) {
+            return -1;
+        }
+        if (this.type().compareTo(otherDuck.type()) > 0) {
+            return 1;
+        }
+        return 0;
     }
 
 }
